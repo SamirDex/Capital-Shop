0d24b23bcdcd0ee8064b87f1819c0fd8f1491620
SamirDex <hebibovsamir26@gmail.com>
2024-09-20 21:36:58 +0400

    category

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,67 +1,139 @@
-import React from 'react'
-import styles from "./Products.module.css"
-import { Link } from 'react-router-dom'
-import { base_url } from '../../data/Data'
-import Card from '../user/components/Card'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react'; 
-import { FaChevronDown } from "react-icons/fa";
-import { FaChevronUp } from "react-icons/fa";
+import styles from './Products.module.css';
+import { Link } from 'react-router-dom';
+import { base_url } from '../../data/Data';
+import Card from '../user/components/Card';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 const categories = [
-    [
-        "Men's Clothing",
-        "Women's Clothing",
-        "Baby's clothing",
-        "Accessory"
-    ],
+    "Men's Clothing",
+    "Women's Clothing",
+    "Baby's Clothing",
+    "Accessory"
+];
+const size = [
+    "Small",
+    "Medium",
+    "Large"
+];
+const color = [
+    "Multi-colored",
+    "Black",
+    "Green",
+    "Red",
+    "White",
+    "Blue",
+    "Gray",
+    "Brown"
 ];
 
 function Products() {
     const [products, setProducts] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState("");
-    const [isOpen, setIsOpen] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState({
+        category: '',
+        color: '',
+        size: '',
+    });
+    const [openCategory, setOpenCategory] = useState('');
 
-    const handleCategoryClick = (category) => {
-        setSelectedCategory(category);
-        setIsOpen(false);
+    const handleOpenCategory = (type) => {
+        setOpenCategory((prev) => (prev === type ? '' : type));
     };
+
+    const handleCategoryClick = (category, type) => {
+        setSelectedCategory((prev) => ({
+            ...prev,
+            [type]: category,
+        }));
+        setOpenCategory('');
+    };
+
     useEffect(() => {
-        axios(base_url + "products").then(res => setProducts(res.data)); 
-    }, [])
-
-    const toggleDropdown = () => {
-        setIsOpen((prev) => !prev);
-    };
-
-    
+        axios(`${base_url}products`).then(res => setProducts(res.data));
+    }, []);
 
     return (
         <div className={styles.container}>
             <div className={styles.header}>
                 <h1>Products</h1>
-                <div className={styles.links}><Link to='/' className={styles.link}>Home</Link> <hr className={styles.hr} /><Link to="/products" className={styles.link}>Products</Link></div>
+                <div className={styles.links}>
+                    <Link to='/' className={styles.link}>Home</Link>
+                    <hr className={styles.hr} />
+                    <Link to="/products" className={styles.link}>Products</Link>
+                </div>
             </div>
             <div className={styles.products}>
                 <div className={styles.left}>
                     <div className={styles.content}>
                         <div className={styles.categories}>
                             <div className={styles.category}>
-                                <div className={styles.dropdown} onClick={() => setIsOpen(!isOpen)}>
-                                <div className={styles.selected} onClick={toggleDropdown}>
-                                        <span>{selectedCategory || "Select Category"}</span>
-                                        <span className={`${styles.arrow} ${isOpen ? styles.open : ''}`}>
-                                            {isOpen ? <FaChevronUp style={{color:"#bab9b5"}}/> : <FaChevronDown style={{color:"#bab9b5"}}/>}
+                                <div className={styles.dropdown} onClick={() => handleOpenCategory("category")}>
+                                    <div className={styles.selected}>
+                                        <span>{selectedCategory.category || "Select Category"}</span>
+                                        <span className={`${styles.arrow} ${openCategory === 'category' ? styles.open : ''}`}>
+                                            {openCategory === 'category' ? <FaChevronUp style={{ color: "#bab9b5" }} /> : <FaChevronDown style={{ color: "#bab9b5" }} />}
                                         </span>
                                     </div>
-                                    <ul className={`${styles.list} ${isOpen ? styles.open : ''}`}>
-                                        {categories[0].map((category, index) => (
+                                    <ul className={`${styles.list} ${openCategory === 'category' ? styles.open : ''}`}>
+                                        {categories.map((cat, index) => (
                                             <li
                                                 key={index}
-                                                onClick={() => handleCategoryClick(category)}
-                                                className={selectedCategory === category ? "active" : ""}
+                                                onClick={() => {
+                                                    handleCategoryClick(cat, "category");
+                                                    handleOpenCategory("category");
+                                                }}
+                                                className={selectedCategory.category === cat ? "active" : ""}
                                             >
-                                                {category}
+                                                {cat}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className={styles.category}>
+                                <div className={styles.dropdown} onClick={() => handleOpenCategory("size")}>
+                                    <div className={styles.selected}>
+                                        <span>{selectedCategory.size || "Select Size"}</span>
+                                        <span className={`${styles.arrow} ${openCategory === 'size' ? styles.open : ''}`}>
+                                            {openCategory === 'size' ? <FaChevronUp style={{ color: "#bab9b5" }} /> : <FaChevronDown style={{ color: "#bab9b5" }} />}
+                                        </span>
+                                    </div>
+                                    <ul className={`${styles.list} ${openCategory === 'size' ? styles.open : ''}`}>
+                                        {size.map((s, index) => (
+                                            <li
+                                                key={index}
+                                                onClick={() => {
+                                                    handleCategoryClick(s, "size");
+                                                    handleOpenCategory("size");
+                                                }}
+                                                className={selectedCategory.size === s ? "active" : ""}
+                                            >
+                                                {s}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className={styles.category}>
+                                <div className={styles.dropdown} onClick={() => handleOpenCategory("color")}>
+                                    <div className={styles.selected}>
+                                        <span>{selectedCategory.color || "Select Color"}</span>
+                                        <span className={`${styles.arrow} ${openCategory === 'color' ? styles.open : ''}`}>
+                                            {openCategory === 'color' ? <FaChevronUp style={{ color: "#bab9b5" }} /> : <FaChevronDown style={{ color: "#bab9b5" }} />}
+                                        </span>
+                                    </div>
+                                    <ul className={`${styles.list} ${openCategory === 'color' ? styles.open : ''}`}>
+                                        {color.map((col, index) => (
+                                            <li
+                                                key={index}
+                                                onClick={() => {
+                                                    handleCategoryClick(col, "color");
+                                                    handleOpenCategory("color"); 
+                                                }}
+                                                className={selectedCategory.color === col ? "active" : ""}
+                                            >
+                                                {col}
                                             </li>
                                         ))}
                                     </ul>
@@ -73,21 +145,21 @@ function Products() {
                     </div>
                 </div>
                 <div className={styles.right}>
-                {
-                        products.length > 0 ? (
-                            products.map((prod) => (
-                                <Card key={prod.id}
+                    {products.length > 0 ? (
+                        products.map((prod) => (
+                            <Card
+                                key={prod.id}
                                 name={prod.name}
                                 img={prod.img}
                                 price={prod.price}
-                                withoutDiscount={prod.withoutDiscount}></Card>
-                            ))
-                        ):<p>Loading products...</p>
-                    }
+                                withoutDiscount={prod.withoutDiscount}
+                            />
+                        ))
+                    ) : <p>Loading products...</p>}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Products
+export default Products;
