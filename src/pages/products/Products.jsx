5a51b3f77678dc71da986e5c15c8dedd5a18e991
SamirDex <hebibovsamir26@gmail.com>
2024-09-20 17:16:19 +0400

    category

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -5,25 +5,72 @@ import { base_url } from '../../data/Data'
 import Card from '../user/components/Card'
 import axios from 'axios';
 import { useState, useEffect } from 'react'; 
+import { FaChevronDown } from "react-icons/fa";
+import { FaChevronUp } from "react-icons/fa";
 
+const categories = [
+    [
+        "Men's Clothing",
+        "Women's Clothing",
+        "Baby's clothing",
+        "Accessory"
+    ],
+];
 
 function Products() {
     const [products, setProducts] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
 
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+        setIsOpen(false);
+    };
     useEffect(() => {
         axios(base_url + "products").then(res => setProducts(res.data)); 
     }, [])
+
+    const toggleDropdown = () => {
+        setIsOpen((prev) => !prev);
+    };
+
     
 
     return (
         <div className={styles.container}>
             <div className={styles.header}>
-                <h1>Contact</h1>
+                <h1>Products</h1>
                 <div className={styles.links}><Link to='/' className={styles.link}>Home</Link> <hr className={styles.hr} /><Link to="/products" className={styles.link}>Products</Link></div>
             </div>
             <div className={styles.products}>
                 <div className={styles.left}>
-                    
+                    <div className={styles.content}>
+                        <div className={styles.categories}>
+                            <div className={styles.category}>
+                                <div className={styles.dropdown} onClick={() => setIsOpen(!isOpen)}>
+                                <div className={styles.selected} onClick={toggleDropdown}>
+                                        <span>{selectedCategory || "Select Category"}</span>
+                                        <span className={`${styles.arrow} ${isOpen ? styles.open : ''}`}>
+                                            {isOpen ? <FaChevronUp style={{color:"#bab9b5"}}/> : <FaChevronDown style={{color:"#bab9b5"}}/>}
+                                        </span>
+                                    </div>
+                                    <ul className={`${styles.list} ${isOpen ? styles.open : ''}`}>
+                                        {categories[0].map((category, index) => (
+                                            <li
+                                                key={index}
+                                                onClick={() => handleCategoryClick(category)}
+                                                className={selectedCategory === category ? "active" : ""}
+                                            >
+                                                {category}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.filter}></div>
+                        <div className={styles.sort}></div>
+                    </div>
                 </div>
                 <div className={styles.right}>
                 {
--- src/pages/user/layout/slider/Slider.jsx
+++ src/pages/user/layout/slider/Slider.jsx
@@ -11,6 +11,7 @@ import hero2 from "../../../../images/h1_hero2.jpg"
 import item1 from "../../../../images/items1.jpg"
 import item2 from "../../../../images/items2.jpg"
 import item3 from "../../../../images/items3.jpg"
+import { Link } from 'react-router-dom';
 
 function Slider() {
     return (
@@ -28,7 +29,7 @@ function Slider() {
                             <span>Fashion Sale</span>
                             <h1>Minimal Menz Style</h1>
                             <p>Consectetur adipisicing elit. Laborum fuga incidunt laboriosam voluptas iure, delectus dignissimos facilis neque nulla earum.</p>
-                            <a href="" className={styles.shop}>Shop Now</a>
+                            <Link to="/products" className={styles.shop}>Shop Now</Link>
 
                         </div>
                         <img src={hero1} alt="" />
@@ -41,7 +42,7 @@ function Slider() {
                             <span>Fashion Sale</span>
                             <h1>Minimal Menz Style</h1>
                             <p>Consectetur adipisicing elit. Laborum fuga incidunt laboriosam voluptas iure, delectus dignissimos facilis neque nulla earum.</p>
-                            <a href="">Shop Now</a>
+                            <Link to="/products">Shop Now</Link>
                         </div>
                         <img src={hero2} alt="" />
                     </div>
@@ -50,17 +51,17 @@ function Slider() {
             <div className={styles.container}>
                 <div className={styles.image}>
                     <h1>Men's Fashion</h1>
-                    <a href="">Shop Now</a>
+                    <Link to="/products">Shop Now</Link>
                     <img src={item1} alt="" />
                 </div>
                 <div className={styles.image}>
                     <h1>Women's Fashion</h1>
-                    <a href="">Shop Now</a>
+                    <Link to="/products">Shop Now</Link>
                     <img src={item2} alt="" />
                 </div>
                 <div className={styles.image}>
                     <h1>Baby Fashion</h1>
-                    <a href="">Shop Now</a>
+                    <Link to="/products">Shop Now</Link>
                     <img src={item3} alt="" />
                 </div>
             </div>
